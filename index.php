ef077ce219db1676d710eb2ca6e063aab8f7a46c
Can Ata <canata09074@gmail.com>
2024-11-30 18:01:07 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,12 @@
 <?php
+$ch_post = curl_init();
 
-// cURL oturumunu başlat
-$curl = curl_init();
-
-// Giriş yapmak için URL ve kullanıcı bilgileri
-$url = "https://api.baubuddy.de/index.php/login";  // Giriş API'sinin URL'si
+// POST cURL isteği
+$url_post = 'https://api.baubuddy.de/index.php/login';  // POST isteği yapılacak URL
 
 // cURL seçeneklerini ayarla
-curl_setopt_array($curl, [
-    CURLOPT_URL => $url,
+curl_setopt_array($ch_post, [
+    CURLOPT_URL => $url_post,
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_HTTPHEADER => [
         "Authorization: Basic QVBJX0V4cGxvcmVyOjEyMzQ1NmlzQUxhbWVQYXNz",
@@ -17,52 +15,15 @@ curl_setopt_array($curl, [
     CURLOPT_POST => true,
     CURLOPT_POSTFIELDS => "{\"username\":\"365\", \"password\":\"1\"}",
 ]);
-
-// İstek gönder ve cevabı al
-$response = curl_exec($curl);
-
-
-
-
-
-
-$err = curl_error($curl);
-// cURL oturumunu kapat
-curl_close($curl);
-if ($err) {
-    echo "cURL Error #:" . $err;
+// POST isteğini gönder
+$response_post = curl_exec($ch_post);
+curl_close($ch_post);
+if (curl_errno($ch_post)) {
+    echo 'Curl error: ' . curl_error($ch_post);
 } else {
-    echo $response;
-    // JSON verisini diziye dönüştür
-    $data = json_decode($response, true);
-
+    $data = json_decode($response_post, true);
     // Store access token in session
     $_SESSION['access_token'] = $data['oauth']['access_token'];
-    
-
-    // HTML Tablosu Başlangıcı
-    echo "<table border='1' cellpadding='10' cellspacing='0'>";
-    echo "<tr><th>Başlık</th><th>Açıklama</th></tr>";
-
-    // OAuth bilgileri
-    echo "<tr><td>Access Token</td><td>" . $data['oauth']['access_token'] . "</td></tr>";
-    echo "<tr><td>Expires In</td><td>" . $data['oauth']['expires_in'] . " saniye</td></tr>";
-    echo "<tr><td>Token Type</td><td>" . $data['oauth']['token_type'] . "</td></tr>";
-    echo "<tr><td>Refresh Token</td><td>" . $data['oauth']['refresh_token'] . "</td></tr>";
-
-    // Kullanıcı Bilgileri
-    echo "<tr><td>Personal No</td><td>" . $data['userInfo']['personalNo'] . "</td></tr>";
-    echo "<tr><td>First Name</td><td>" . $data['userInfo']['firstName'] . "</td></tr>";
-    echo "<tr><td>Last Name</td><td>" . $data['userInfo']['lastName'] . "</td></tr>";
-    echo "<tr><td>Display Name</td><td>" . $data['userInfo']['displayName'] . "</td></tr>";
-    echo "<tr><td>Active</td><td>" . ($data['userInfo']['active'] ? 'Evet' : 'Hayır') . "</td></tr>";
-    echo "<tr><td>Business Unit</td><td>" . $data['userInfo']['businessUnit'] . "</td></tr>";
-
-    // API Version
-    echo "<tr><td>API Version</td><td>" . $data['apiVersion'] . "</td></tr>";
-
-    // Tablo Kapanışı
-    echo "</table>";
 }
 ?>
 
